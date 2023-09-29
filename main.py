395e01c4baceaf0b01892c84de1df9666f4ac7c0
pirabanjan <pirabanjan@gmail.com>
2023-09-28 23:30:12 -0400

    Final app

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
-# from src.predict import predict_image
-
-
-# # Call the predict_image function with your image data
-# # Replace 'image_path' with the path to your image file
-# image_path = 'classification_project\classification_project\Image\gfx100s_sample_04_thum-1.jpg'
-# predicted_class = predict_image(image_path)
-# print(f"Predicted class: {predicted_class}")
+import os
+import time
 from src.predict import predict_image
 from src.train import train_model
 
-# Call the predict_image function with your image data
-# Replace 'image_path' with the path to your image file
-image_path = '3345-IAC1\gfx100s_sample_04_thum-1.jpg'
-predicted_class = predict_image(train_model, image_path)
+# Get the directory where this script is located
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Specify the directory containing the images relative to the script's location
+image_directory = os.path.join(script_directory, 'Image')
 
 # Specify the output file path
 output_file_path = 'output.txt'  # Change this to your desired output file path
 
-# Write the predicted class to the output file
-with open(output_file_path, 'w') as output_file:
-    output_file.write(f"Predicted class: {predicted_class}")
+# Initialize a set to keep track of processed file names
+processed_files = set()
 
-print(f"Predicted class: {predicted_class}")
+while True:
+    # Get the list of image files in the directory with .jpg, .png, and .jpeg extensions
+    image_files = [filename for filename in os.listdir(image_directory) if filename.lower().endswith(('.jpg', '.png', '.jpeg'))]
+    
+    # Filter out files that have already been processed
+    new_files = [filename for filename in image_files if filename not in processed_files]
+    
+    if new_files:
+        # Initialize an empty list to store predicted classes and their corresponding file names
+        predicted_classes = []
+
+        # Iterate over the new image files
+        for filename in new_files:
+            # Construct the full path to the image file
+            image_path = os.path.join(image_directory, filename)
+
+            # Call the predict_image function with the image data
+            predicted_class = predict_image(train_model, image_path)
+
+            # Append the predicted class and filename to the list
+            predicted_classes.append((filename, predicted_class))
+
+            # Add the filename to the set of processed files
+            processed_files.add(filename)
+
+        # Write the predicted classes to the output file
+        with open(output_file_path, 'a') as output_file:
+            for filename, predicted_class in predicted_classes:
+                output_file.write(f"File: {filename}, Predicted class: {predicted_class}\n")
+
+        # Print the results
+        for filename, predicted_class in predicted_classes:
+            print(f"File: {filename}, Predicted class: {predicted_class}")
+
+    # Sleep for a while before checking again (adjust the sleep duration as needed)
+    time.sleep(10)  # Sleep for 10 seconds before checking again
