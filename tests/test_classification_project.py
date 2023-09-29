ed3596359a563bbcab3440f8ba2450fb67009478
pirabanjan <pirabanjan@gmail.com>
2023-09-29 00:34:08 -0400

    Final app 8

--- tests/test_classification_project.py
+++ tests/test_classification_project.py
@@ -20,10 +20,26 @@ num_samples_for_testing = 1000
 def cifar10_data():
     x_train, y_train, x_test, y_test = load_cifar10_data()
     x_test_subset, y_test_subset = x_test[:num_samples_for_testing], y_test[:num_samples_for_testing]
+    # return x_train, y_train, x_test, y_test
     return x_train, y_train, x_test_subset, y_test_subset
 
+# @pytest.fixture
+# def cifar10_data():
+#     x_train, y_train, x_test, y_test = load_cifar10_data()
+#     return x_train[:100], y_train[:100], x_test[:20], y_test[:20]  # Adjust the slice sizes as needed
+
+
+# @pytest.fixture
+# def cifar10_model(cifar10_data):
+#     x_train, y_train, x_test, y_test = cifar10_data
+#     model = create_cnn_model(x_train[0].shape)
+#     train_model(model, x_train, y_train,x_test, y_test, epochs=1, batch_size=64)
+#     #model = train_model()
+#     return model
 @pytest.fixture
 def cifar10_model(cifar10_data):
+    # x_train, y_train, x_test, y_test = cifar10_data
+    # model = create_cnn_model(x_train[0].shape)
     train_model()
     model = train_model()
     return model
@@ -37,26 +53,47 @@ def test_data_preparation(cifar10_data):
     assert y_train.shape[1] == 10
     assert y_test.shape[1] == 10
 
+# def test_model_creation(cifar10_model):
+#     assert isinstance(cifar10_model, tf.keras.models.Sequential)
 def test_model_creation():
+    # Create the model
     model = create_cnn_model(input_shape=(32, 32, 3))
-    assert isinstance(model, tf.keras.models.Sequential)
-    assert model.layers
 
+    # Define your assertion(s)
+    assert isinstance(model, tf.keras.models.Sequential)
+    assert model.layers  
+
+# def test_model_training(cifar10_data, cifar10_model):
+#     x_train, y_train, x_test, y_test = cifar10_data
+#     _, accuracy = cifar10_model.evaluate(x_test, y_test)
+#     assert accuracy > 0.4  # Adjust as needed
 def test_model_training(cifar10_data, cifar10_model):
+    # Make sure cifar10_model is not None
+    # assert cifar10_model is not None
+
     x_train, y_train, x_test, y_test = cifar10_data
     if cifar10_model:
         _, accuracy = cifar10_model.evaluate(x_test, y_test)
         assert accuracy > 0.7  # Adjust as needed
 
+
+# def test_model_prediction(cifar10_model):
+#     # Generate a random test image for prediction
+#     test_image = np.random.rand(32, 32, 3)
+#     predicted_class = predict_image(cifar10_model, test_image)
+#     assert 0 <= predicted_class < 10
+# def test_model_prediction(cifar10_model):
+#     # Generate a random test image for prediction
+#     test_image = np.random.rand(1, 32, 32, 3)  # Add an extra dimension for batch size
+#     predicted_class = predict_image(cifar10_model, test_image)
+#     assert 0 <= predicted_class < 10
 def test_model_prediction(cifar10_model):
-    test_image = np.random.rand(1, 32, 32, 3).astype(np.float32)
+    # Generate a smaller random test image for prediction
+    # test_image = np.random.rand(1, 32, 32, 3).astype(np.float32)  # Use float32 data type
+    test_image = np.random.rand(1, 32, 32, 3).astype(np.float32) 
     predicted_class = predict_image(cifar10_model, test_image)
     assert 0 <= predicted_class < 10
 
+
 if __name__ == '__main__':
-    # Redirect output to a file and display it in the console
-    output_file = open('test_output.txt', 'w')
-    pytest_args = ["-o", "tee", "test_output.txt"]
-    pytest_args.extend(sys.argv[1:])  # Include any command-line arguments passed to the script
-    pytest.main(pytest_args, plugins=[output_file])
-    output_file.close()
+    pytest.main()
